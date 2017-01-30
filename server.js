f22071eb135c04c4c3dfa766e4756d7be2d2d8fc
Summer Guan <summerguan@Summers-MBP.fios-router.home>
2017-01-30 11:14:36 -0500

    change port setting

--- server.js
+++ server.js
@@ -22,3 +22,7 @@ app.use('/products', require('./products/products.routes.js'));
 app.get('/',(req,res,next)=>{
     res.render('index',{nav:'home',title:'Home'});
 })
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
